2acaeca7b23e9b393f9815bfab6ba51c8cf26bae
junior <schwarzer.13@rede.ulbra.br>
2020-06-23 21:22:22 -0300

    ola

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+public class cadeira
+{
+    private professor a;
+    private String nomeDaCadeira;
+    private String descricaoDaCadeira;
+    private int cargaHoraria;
+    private int horaInicio;
+    private int horaFim;
+    private int comecoDia;
+    private int comecoMes;
+    private int comecoAno;
+}
+
+public class pessoa {
+    private String nome;
+    private int idade;
+    private String cpf;
+    private int cep;
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    
 }
